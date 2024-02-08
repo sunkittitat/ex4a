28328bb2a3be6ceee8b6d22def73f6e217d3ddac
sunkittitat <tanapattanalux@gmail.com>
2024-02-08 15:46:32 +0700

    refractor#2 - add validate fn.

--- common.js
+++ common.js
@@ -8,20 +8,24 @@ function validation(pace){
 		return {"status":0,"message":msg,"hour":0,"min":0};
 	}
 }
+function paceCal(pace,dist){
+	var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+		}
+		return {"min":min,"house":hour};
+}
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
 	//---------------------------
 		var chk = validate(pace);
-		if(chk !=null) return chk;
+		if(chk != null) return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
